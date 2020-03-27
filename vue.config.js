cb54f77cfb52defaa1dbdfd4efb05133dc1af1bf
ics-matsumoto <matsumoto@ics-web.jp>
2020-03-27 18:41:33 +0900

    github用にビルドを設定

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
 module.exports = {
-  indexPath: '20_performance.html',
-  outputDir: 'docs'
+  pages: {
+    index: {
+      // entry for the page
+      entry: 'src/main.js',
+      // the source template
+      template: 'public/20_performance.html',
+      // output as dist/index.html
+      filename: '20_performance.html',
+      // when using title option,
+      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+      title: 'Shadow Performance Test'
+    }
+  },
+  outputDir: 'docs',
+  publicPath: './'
 }
